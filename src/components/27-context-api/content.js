7c0f059d2a0b2b304381856202b9fdc075e0f9bb
Mehmet Emin Çatal <mhmtmnctl20@gmail.com>
2023-01-18 23:28:17 +0300

    currencies

--- src/components/27-context-api/content.js
+++ src/components/27-context-api/content.js
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import { Form, InputGroup } from "react-bootstrap";
 const Content = () => {
   return (
-    <div>Content</div>
-  )
-}
-
-export default Content
+    <div className="p-5">
+      <InputGroup className="mb-3">
+        <Form.Control />
+        <Form.Select>
+          <option value="1">USD</option>
+          <option value="2">EUR</option>
+        </Form.Select>
+        <InputGroup.Text id="basic-addon1">Result:</InputGroup.Text>
+      </InputGroup>
+    </div>
+  );
+};
+export default Content;
