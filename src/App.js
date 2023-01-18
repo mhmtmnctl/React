4c3f7ee35de502d3236733ff1f66c58c14b40c41
Mehmet Emin Çatal <mhmtmnctl20@gmail.com>
2023-01-18 21:05:11 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/00-home/header/header";
+import Menu from "./components/00-home/menu/menu";
 import HelloWorld from "./components/01-hello-world/hello-world";
+import HelloReact from "./components/02-hello-react/hello-react";
 import Jsx1 from "./components/03-jsx/jsx1";
 import Jsx2 from "./components/03-jsx/jsx2";
 import Jsx3 from "./components/03-jsx/jsx3";
@@ -40,60 +45,70 @@ import Form1 from "./components/26-forms/form1";
 import Form2 from "./components/26-forms/form2";
 import Form3 from "./components/26-forms/form3";
 import Form4 from "./components/26-forms/form4";
+import Form5 from "./components/26-forms/form5";
+import Form6 from "./components/26-forms/form6";
 
 const App = () => {
-
-
   return (
-    <div>
-      {/*         
-        <HelloWorld/>
-        <Jsx1/>
-        <Jsx2/>
-        <Jsx3/>
-        <Jsx4/>
-        <Jsx5/>
-        <Jsx6 />
-        <Jsx7/>
-        <Style1/>
-        <Style2/>
-        <Style3/>
-        <Style4/>
-        <Clock1/>
-        <Greetings/>
-        <Products/>
-        <Clock2 textColor="#56ff33" bgColor="#0012C3"/>
-        <Image/>
-        <Gallery/>
-        <ProfileCard avatar="profile.jpg" name="Ali Gel" location="Turkey, Istanbul" shot="3" followers="23432" following="234"/>
-        <BootstrapStatic/>
-        <BootstrapDynamic/>
-        <Icon/>
-        <Events/>
-        <Shop/>
-        <Stateless/>
-        <State/>
-        <Counter/>
-        <Birthday/>
-        <UseEffect1/>
-        <Clock3/>
-        <UseEffect2/>
-        <CountryFilter/>
-        <UseRef/>
-        <Scroll/>
-        <ClassComp/>
-        <UserCards/>
-        <Countries/>
-        <ParentComp/>
-        <Form1/>
-        <Form2/>
-        <Form3/>
- */}
-        
-        <Form4/>
-        
-        
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Container fluid>
+        <Row>
+          <Col sm={2}>
+            <Menu />
+          </Col>
+          <Col sm={10}>
+            <Routes>
+              <Route path="/hello-world" element={<HelloWorld />} />
+              <Route path="/hello-react" element={<HelloReact />} />
+              <Route path="/jsx1" element={<Jsx1 />} />
+              <Route path="/jsx2" element={<Jsx2 />} />
+              <Route path="/jsx3" element={<Jsx3 />} />
+              <Route path="/jsx4" element={<Jsx4 />} />
+              <Route path="/jsx5" element={<Jsx5 />} />
+              <Route path="/jsx6" element={<Jsx6 />} />
+              <Route path="/jsx7" element={<Jsx7 />} />
+              <Route path="/style1" element={<Style1 />} />
+              <Route path="/style2" element={<Style2 />} />
+              <Route path="/style3" element={<Style3 />} />
+              <Route path="/style4" element={<Style4 />} />
+              <Route path="/clock1" element={<Clock1 />} />
+              <Route path="/clock2" element={<Clock2 textColor="#56ff33" bgColor="#0012c3" />}/>
+              <Route path="/clock3" element={<Clock3 />} />
+              <Route path="/greetings" element={<Greetings />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/image" element={<Image />} />
+              <Route path="/gallery" element={<Gallery />} />
+              <Route path="/profilecard" element={<ProfileCard avatar="profile.jpg" name="Ali Gel" location="Turkey, Istanbul" shot="3" followers="23432" following="234"/>}/>
+              <Route path="/bootstrapstatic" element={<BootstrapStatic />} />
+              <Route path="/bootstrapdynamic" element={<BootstrapDynamic />} />
+              <Route path="/icon" element={<Icon />} />
+              <Route path="/events" element={<Events />} />
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/stateless" element={<Stateless />} />
+              <Route path="/state" element={<State />} />
+              <Route path="/counter" element={<Counter />} />
+              <Route path="/birthday" element={<Birthday />} />
+              <Route path="/useeffect1" element={<UseEffect1 />} />
+              <Route path="/useeffect2" element={<UseEffect2 />} />
+              <Route path="/countryfilter" element={<CountryFilter />} />
+              <Route path="/useref" element={<UseRef />} />
+              <Route path="/scroll" element={<Scroll />} />
+              <Route path="/classcomp" element={<ClassComp />} />
+              <Route path="/usercards" element={<UserCards />} />
+              <Route path="/countries" element={<Countries />} />
+              <Route path="/parentcomp" element={<ParentComp />} />
+              <Route path="/form1" element={<Form1 />} />
+              <Route path="/form2" element={<Form2 />} />
+              <Route path="/form3" element={<Form3 />} />
+              <Route path="/form4" element={<Form4 />} />
+              <Route path="/form5" element={<Form5 />} />
+              <Route path="/form6" element={<Form6 />} />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
+    </BrowserRouter>
   );
 };
 export default App;
